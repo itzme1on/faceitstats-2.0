00c278eb93685c3de58712853417d27dd58aeddb
Venikov Danil <melongames@mail.ru>
2021-10-10 18:32:15 +0200

    Edit intents

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { readdirSync } = require('fs')
 const languages = require('./languages/language')
 const { Client, Collection, Intents, MessageEmbed } = require('discord.js')
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.DIRECT_MESSAGES]})
+const client = new Client({ intents: [Intents.FLAGS.GUILDS]})
 client.commands = new Collection()
 
 const eventFiles = readdirSync('./events/').filter(file => file.endsWith('js'))
